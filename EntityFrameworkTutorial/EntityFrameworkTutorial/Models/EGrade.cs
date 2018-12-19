b31f79cbd1f94a6b93e29d0a78f8d029ca872c62
Ivan Dario Villota <idvillota@gmail.com>
2018-12-19 14:15:22 -0500

    Create the solution.

---
+++ EntityFrameworkTutorial/EntityFrameworkTutorial/Models/EGrade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTutorial.Models
+{
+    public enum EGrade
+    {
+        A, B, C, D, E, F
+    }
+}
---
+++ EntityFrameworkTutorial/EntityFrameworkTutorial/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿namespace EntityFrameworkTutorial.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+
+        public int CourseID { get; set; }
+
+        public int StudentID { get; set; }
+
+        public EGrade? Grade { get; set; }
+
+        public Course Course { get; set; }
+
+        public Student Student { get; set; }
+    }
+}
